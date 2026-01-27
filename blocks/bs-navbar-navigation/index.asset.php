42d8b98ca4205e395bbad82a77b62e2418409f5e
ve3 <kawewong@gmail.com>
2026-01-27 15:34:13 +0700

    Removed blocks, move them to plugin 'bbfse-plugin'.

--- blocks/bs-navbar-navigation/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-server-side-render', 'wp-url'), 'version' => '4a66eedf3d9b94d94d19');
--- inc/classes/WPOverride/readme.txt
+++
@@ -1,3 +0,0 @@
-WPOverride is for store class files that has been copied the source code from WordPress core and modify its functional.
-
-In some case, devs cannot use hooks to alter functional.
