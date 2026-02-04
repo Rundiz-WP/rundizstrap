bdebebb40dc9753caf59c1c62ce442ee61f0f9df
ve3 <kawewong@gmail.com>
2026-02-05 01:48:58 +0700

    New pattern post navigation.

---
+++ patterns/post-navigation.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Post navigation
+ * Slug: bootstrap-basic-fse/post-navigation
+ * Categories: text
+ * Description: Next and previous post links.
+ * Block Types: core/post-navigation-link
+ *
+ * @package bootstrap-basic-fse
+ * @since 0.0.1
+ */
+
+?>
+
+<!-- wp:bbfse-plugin/blocks-bs-row {"className":"gy-2 gy-md-0 mt-5 border-top pt-5 mb-5"} -->
+<div class="wp-block-bbfse-plugin-blocks-bs-row row gy-2 gy-md-0 mt-5 border-top pt-5 mb-5">
+    <!-- wp:bbfse-plugin/blocks-bs-column {"className":"col-12 col-md-6 text-center text-md-start"} -->
+    <div class="wp-block-bbfse-plugin-blocks-bs-column col-12 col-md-6 text-center text-md-start">
+        <!-- wp:bbfse-plugin/blocks-bs-post-navigation-link {"type":"previous","showTitle":true,"prependTextHtml":"\u003ci class=\u0022bi bi-chevron-left\u0022\u003e\u003c/i\u003e","className":"btn btn-outline-secondary"} /-->
+    </div>
+    <!-- /wp:bbfse-plugin/blocks-bs-column -->
+
+    <!-- wp:bbfse-plugin/blocks-bs-column {"className":"col-12 col-md-6 text-center text-md-end"} -->
+    <div class="wp-block-bbfse-plugin-blocks-bs-column col-12 col-md-6 text-center text-md-end">
+        <!-- wp:bbfse-plugin/blocks-bs-post-navigation-link {"showTitle":true,"appendTextHtml":"\u003ci class=\u0022bi bi-chevron-right\u0022\u003e\u003c/i\u003e","className":"btn btn-outline-secondary"} /-->
+    </div>
+    <!-- /wp:bbfse-plugin/blocks-bs-column -->
+</div>
+<!-- /wp:bbfse-plugin/blocks-bs-row -->
