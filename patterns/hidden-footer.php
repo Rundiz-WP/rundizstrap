7bdc7ccd46bb549551f1717a1834bcfcdbda9dfc
ve3 <kawewong@gmail.com>
2026-02-05 02:00:56 +0700

    Update patterns/hidden-footer.php

--- patterns/hidden-footer.php
+++ patterns/hidden-footer.php
@@ -13,15 +13,22 @@
 <div class="wp-block-bbfse-plugin-blocks-bs-container container mb-3">
     <!-- wp:bbfse-plugin/blocks-bs-row {"className":"g-0"} -->
     <div class="wp-block-bbfse-plugin-blocks-bs-row row g-0">
-        <!-- wp:bbfse-plugin/blocks-bs-column {"className":"col border-top border-5 pt-3 d-flex justify-content-between fw-light text-body-secondary"} -->
-        <div class="wp-block-bbfse-plugin-blocks-bs-column col border-top border-5 pt-3 d-flex justify-content-between fw-light text-body-secondary">
-            <!-- wp:site-title {"level":0,"className":"p-0 m-0"} /-->
-
-            <!-- wp:paragraph {"className":"p-0 m-0"} -->
-            <p class="p-0 m-0"><?php printf(
+        <!-- wp:bbfse-plugin/blocks-bs-column {"className":"col border-top border-5 pt-3 d-flex justify-content-start fw-light text-body-secondary"} -->
+        <div class="wp-block-bbfse-plugin-blocks-bs-column col border-top border-5 pt-3 d-flex justify-content-start fw-light text-body-secondary">
+            <!-- wp:paragraph {"className":"p-0 my-0"} -->
+            <p class="p-0 my-0"><?php printf(
                 /* translators: %1$s open link tag, %2$s close link tag. */
                 esc_html__('Powered by %1$sWordPress%2$s.', 'bootstrap-basic-fse'),
                 '<a class="link-secondary" href="https://wordpress.org" target="wordpress">',
+                '</a>'
+            ); ?></p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"className":"p-0 my-0 ms-1"} -->
+            <p class="p-0 my-0 ms-1"><?php printf(
+                /* translators: %1$s open link tag, %2$s close link tag. */
+                esc_html__('Theme: %1$sBootstrap Basic FSE%2$s.', 'bootstrap-basic-fse'),
+                '<a class="link-secondary" href="https://rundiz.com" target="rundiz-com">',
                 '</a>'
             ); ?></p>
             <!-- /wp:paragraph -->
