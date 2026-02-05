f27c0f2140333fabf4062389b7851fceb5fdf3cf
ve3 <kawewong@gmail.com>
2026-02-05 13:48:48 +0700

    Add detect required plugin.

---
+++ inc/classes/Hooks/DetectRequiredPlugin.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Bootstrap Basic FSE - Detect required plugin.
+ * 
+ * @package bootstrap-basic-fse
+ * @since 0.0.1
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+
+namespace BootstrapBasicFSE\Hooks;
+
+
+if (!class_exists('\\BootstrapBasicFSE\\Hooks\\DetectRequiredPlugin')) {
+    /**
+     * Detect required plugin class.
+     * 
+     * @since 0.0.1
+     */
+    class DetectRequiredPlugin implements \BootstrapBasicFSE\Interfaces\AutoRegisterInterface
+    {
+
+
+        /**
+         * Detect required plugin and display alert if not found activated.
+         * 
+         * @since 0.0.1
+         */
+        public function detectAndDisplayAlert()
+        {
+            if (!class_exists('\\BBFSEPlugin\\App\\App')) {
+                $Loader = new \BootstrapBasicFSE\Libraries\Loader();
+                $Loader->loadView('Hooks/detectRequiredPlugin_v');
+                unset($Loader);
+            }// endif;
+        }// detectAndDisplayAlert
+
+
+        /**
+         * Enqueue styles and scripts.
+         * 
+         * @since 0.0.1
+         */
+        public function enqueueStylesAndScripts()
+        {
+            wp_enqueue_script('bootstrap-basic-fse-hooks-detect-required-plugin', get_theme_file_uri('assets/js/Hooks/detect-required-plugin.js'), [], BOOTSTRAPBASICFSE_VERSION, true);
+        }// enqueueStylesAndScripts
+
+
+        /**
+         * {@inheritDoc}
+         * 
+         * @since 0.0.1
+         */
+        public function registerHooks()
+        {
+            add_action('wp_enqueue_scripts', [$this, 'enqueueStylesAndScripts']);
+            add_action('wp_footer', [$this, 'detectAndDisplayAlert']);
+        }// registerHooks
+
+
+    }// DetectRequiredPlugin
+}
