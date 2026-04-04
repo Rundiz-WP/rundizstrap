3e73ed02f27d4b22baa78e771346f8d0fa566ff6
ve3 <kawewong@gmail.com>
2026-04-04 20:09:27 +0700

    Update post navigation pattern to wrap long post title.

--- patterns/post-navigation.php
+++ patterns/post-navigation.php
@@ -17,13 +17,13 @@
 <div class="wp-block-rundizstrap-companion-blocks-bs-row row gx-0 gy-2 gy-md-0 mt-5 border-top pt-5 mb-5">
     <!-- wp:rundizstrap-companion/blocks-bs-column {"className":"col-12 col-md-6 text-center text-md-start"} -->
     <div class="wp-block-rundizstrap-companion-blocks-bs-column col-12 col-md-6 text-center text-md-start">
-        <!-- wp:rundizstrap-companion/blocks-bs-post-navigation-link {"type":"previous","showTitle":true,"prependTextHtml":"\u003ci class=\u0022bi bi-chevron-left\u0022\u003e\u003c/i\u003e","className":"btn btn-outline-secondary"} /-->
+        <!-- wp:rundizstrap-companion/blocks-bs-post-navigation-link {"type":"previous","showTitle":true,"prependTextHtml":"\u003ci class=\u0022bi bi-chevron-left\u0022\u003e\u003c/i\u003e","className":"btn btn-outline-secondary text-break"} /-->
     </div>
     <!-- /wp:rundizstrap-companion/blocks-bs-column -->
 
     <!-- wp:rundizstrap-companion/blocks-bs-column {"className":"col-12 col-md-6 text-center text-md-end"} -->
     <div class="wp-block-rundizstrap-companion-blocks-bs-column col-12 col-md-6 text-center text-md-end">
-        <!-- wp:rundizstrap-companion/blocks-bs-post-navigation-link {"showTitle":true,"appendTextHtml":"\u003ci class=\u0022bi bi-chevron-right\u0022\u003e\u003c/i\u003e","className":"btn btn-outline-secondary"} /-->
+        <!-- wp:rundizstrap-companion/blocks-bs-post-navigation-link {"showTitle":true,"appendTextHtml":"\u003ci class=\u0022bi bi-chevron-right\u0022\u003e\u003c/i\u003e","className":"btn btn-outline-secondary text-break"} /-->
     </div>
     <!-- /wp:rundizstrap-companion/blocks-bs-column -->
 </div>
